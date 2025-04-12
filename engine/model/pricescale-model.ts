56bdf758598c6c44eaacafbdbc1f77628cb6d694
Damian Kowalski <dkowalski.1997@gmail.com>
2025-04-12 15:07:48 +0200

    added ohlc source

--- engine/model/pricescale-model.ts
+++ engine/model/pricescale-model.ts
@@ -1,10 +1,21 @@
+import { IPriceScaleMetadata } from './types/imodel';
+
 export class PriceScaleModel {
   private _rowDist = 6;
+  private _offset = 0;
 
-  constructor() {
-    setInterval(() => {
-      this._rowDist += 0.01;
-    }, 10);
+  private _pipSize = 1;
+
+  constructor({ pipSize }: IPriceScaleMetadata = { pipSize: 1 }) {
+    this._pipSize = pipSize;
+  }
+
+  /**
+   * The value representing the minimal possible price
+   * change.
+   */
+  get pipSize(): number {
+    return this._pipSize;
   }
 
   /**
@@ -14,5 +25,21 @@ export class PriceScaleModel {
    */
   get rowDist(): number {
     return this._rowDist;
+  }
+
+  /**
+   * Disance in value that represents the distance in value
+   * between two closest rows.
+   */
+  get rowValueDist(): number {
+    return this.pipSize / this.rowDist;
+  }
+
+  /**
+   * Returns the offset in pixels that the user moved
+   * either to top or bottom.
+   */
+  get offset(): number {
+    return this._offset;
   }
 }
--- engine/model/types/imodel.ts
+++ engine/model/types/imodel.ts
@@ -5,3 +5,7 @@ export type IModel = {
   timeScaleModel?: TimeScaleModel;
   priceScaleModel?: PriceScaleModel;
 };
+
+export type IPriceScaleMetadata = {
+  pipSize: number;
+};
